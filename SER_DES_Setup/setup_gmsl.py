2faa92dc2e49624db9d192553cc20724b0bdb916
nplano10 <nathan.plano10@gmail.com>
2025-02-25 14:34:43 -0500

    bug fix

--- SER_DES_Setup/setup_gmsl.py
+++ SER_DES_Setup/setup_gmsl.py
@@ -13,30 +13,15 @@ cam = gmsli2c.GMSLi2c(rpi4)
 cam.LOG = True  # Enable logging
 
 cam.regWrite(HS96, 0x10, 0x80)
-time.sleep(0.2)
+time.sleep(0.5)
 
 
 cam.regWrite(HS97, 0x10, 0x80)
-time.sleep(0.2)
+time.sleep(0.5)
 
 
 
-#cam.parseFile('MAX96717+MAX96716A_tunnel.cpp')
-cam.parseFile('test.cpp')
-
-val = cam.regRead(HS97, 0x4D)
-print(val)
-time.sleep(0.2)
-
-
-val = cam.regRead(HS97, 0x4C)
-print(val)
-time.sleep(0.2)
-
-
-
-
-
+cam.parseFile('MAX96717+MAX96716A_tunnel.cpp')
 
 
 HS96 = 0x98
@@ -46,25 +31,16 @@ cam = gmsli2c.GMSLi2c(rpi4)
 cam.LOG = True  # Enable logging
 
 cam.regWrite(HS96, 0x10, 0x80)
-time.sleep(0.2)
+time.sleep(0.5)
 
 
 cam.regWrite(HS97, 0x10, 0x80)
-time.sleep(0.2)
+time.sleep(0.5)
+
+cam.parseFile('MAX96717+MAX96716A_tunnel.cpp')
 
 
 
-#cam.parseFile('MAX96717+MAX96716A_tunnel.cpp')
-cam.parseFile('test.cpp')
-
-val = cam.regRead(HS97, 0x4D)
-print(val)
-time.sleep(0.2)
-
-
-val = cam.regRead(HS97, 0x4C)
-print(val)
-time.sleep(0.2)
 
 
 
