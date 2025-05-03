f37a4f1e714e0da7f72db515fc9206df8d880188
Du <Fang.Du@sony.com>
2025-05-03 09:34:06 -0700

    Update imx500_object_detection_SORT.py

--- imx500/imx500_object_detection_SORT.py
+++ imx500/imx500_object_detection_SORT.py
@@ -364,9 +364,9 @@ class IMX500Detector:
                 # Add classification result if available
                 bbox_color = (255, 255, 0, 0)
                 if labels[detection.category] == JELLYBEAN:
-                    bbox_color = (255, 0, 0, 0) # red for jellybean
+                    bbox_color = (0, 0, 255, 0) # blue for jellybean
                 if detection.tracking_id in self.anomaly_results:
-                    if not self.anomaly_results[detection.tracking_id]:
+                    if self.anomaly_scores[detection.tracking_id] < self.anomaly_threshold:
                         bbox_color = (0, 255, 0, 0) # green for normal pill
                     else:
                         bbox_color = (255, 0, 0, 0) # red for abnormal pill
